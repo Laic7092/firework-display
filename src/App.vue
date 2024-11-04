c20e7c4513cec22548dffba3d2ca9084b51012eb
Laix <894169287@qq.com>
2024-11-04 09:00:41 +0800

    ghpages

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <video controls width="250">
+      <source src="/test.mp4" type="video/mp4" />
+      Download the
+      <a href="/media/cc0-videos/flower.webm">WEBM</a>
+      or
+      <a href="/media/cc0-videos/flower.mp4">MP4</a>
+      video.
+    </video>
+  </div>
+</template>
