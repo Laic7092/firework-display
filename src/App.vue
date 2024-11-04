fb1c5b38a3cef1a404112ca669a3788b63360cb8
Laix <894169287@qq.com>
2024-11-04 14:16:56 +0800

    rm old mp4

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,20 @@
 
 <template>
   <div>
-    <video controls width="250">
-      <source src="/test.webm" type="video/webm" />
-      <source src="/test.mp4" type="video/mp4" />
-    </video>
+    <article>
+      <header>
+        <h1>视频测试页面</h1>
+      </header>
+    </article>
+    <main>
+      <h2>webm格式</h2>
+      <video controls width="250" playsinline="true" webkit-playsinline="true">
+        <source src="/test.webm" type="video/webm" />
+      </video>
+      <h2>mp4格式</h2>
+      <video controls width="250" playsinline="true" webkit-playsinline="true">
+        <source src="/test1.mp4" type="video/mp4" />
+      </video>
+    </main>
   </div>
 </template>
